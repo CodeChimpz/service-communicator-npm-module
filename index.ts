02c118cb6896af7cf33c3bc106eb28634090153a
taburet <md12072004@gmail.com>
2023-01-30 13:46:45 +0200

    reworks

--- index.ts
+++ index.ts
@@ -1,47 +1,51 @@
 import axios from "axios";
-import {Logger, WinstonLogger} from "logger";
+import {LoggerService, WinstonLoggerService} from "logger";
 
-interface GatewayConfigOptions {
+export interface GatewayConfigOptions {
     url: string
     //how the gateway will refer to the service when registering endpoints
     refer: string
     //api key given by the gateway to the service
     api_key: string
-    endpoints: Array<string>
+    //endpoints
+    endpoints: ApiObjectT
+    //for testing on localhost when Origin header is not sent
+    origin?: string
 }
 
+// export type HttpMethodT = 'get' | 'post' | 'delete' | 'put'
+export type ApiObjectT = {
+    [key: string]: string
+}
+
+
 export class GatewayCommunicator {
-    logger: Logger
+    logger: LoggerService
     config: GatewayConfigOptions
 
     constructor(config: GatewayConfigOptions) {
-        this.logger = new WinstonLogger({
+        this.logger = new WinstonLoggerService({
             path: "./logs",
-            console: true
+            console: true,
+            maxsize: 59999
         })
         this.config = config
     }
 
     //send all the endpoint routes passed on init to the gateway endpoint registration checkpoint
     async sync() {
-        const response = await axios.post(this.config.url + '/endpoints', {
-            auth: this.config.api_key,
-            service: this.config.refer,
-            endpoints: this.config.endpoints
-        })
-        //
-        if (response.status > 300) {
-            throw new Error('Failed to sync with the server: ' + response.data.message)
+        try {
+            const response = await axios.post(this.config.url, {
+                auth: this.config.api_key,
+                service: this.config.refer,
+                endpoints: this.config.endpoints,
+                origin: this.config.origin,
+            }, {})
+            this.logger.http.info(response.data, {status: response.status})
+        } catch (e: any) {
+            this.logger.http.error(e)
         }
     }
 
-    //send data to gateway ( e.g. notify that service is down )
-    async send(data: any) {
-        const response = await axios.post(this.config.url + '/data', {
-            auth: this.config.api_key,
-            data: data
-        })
-        return response.data
-    }
 }
 
